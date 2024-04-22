3175ab8377f1f600673c203f1493e22e94a110d3
kyljmeeski <kyljmeeski@gmail.com>
2024-04-22 20:48:08 +0600

    doc: add javadoc to Task and Scheduler

--- src/main/java/com/kyljmeeski/plainscheduler/Scheduler.java
+++ src/main/java/com/kyljmeeski/plainscheduler/Scheduler.java
@@ -1,13 +1,36 @@
 package com.kyljmeeski.plainscheduler;
 
+/**
+ * Interface for scheduling tasks to execute.
+ * Provides methods to schedule tasks, pause/resume specific tasks, and shut down the scheduler.
+ */
 public interface Scheduler {
 
+    /**
+     * Schedules a task for execution.
+     *
+     * @param task the task to schedule
+     */
     void schedule(Task task);
 
+    /**
+     * Pauses the execution of a scheduled task.
+     *
+     * @param task the task to pause
+     */
     void pause(Task task);
 
+    /**
+     * Resumes the execution of a paused task.
+     *
+     * @param task the task to resume
+     */
     void resume(Task task);
 
+    /**
+     * Shuts down the scheduler, stopping all scheduled tasks.
+     * After shutdown, the scheduler cannot be used to schedule new tasks.
+     */
     void shutdown();
 
 }
--- src/main/java/com/kyljmeeski/plainscheduler/Task.java
+++ src/main/java/com/kyljmeeski/plainscheduler/Task.java
@@ -1,15 +1,41 @@
 package com.kyljmeeski.plainscheduler;
 
+/**
+ * Represents a task that can be scheduled by a Scheduler.
+ * Implementations of Task should provide a job to execute, an interval for execution,
+ * and methods to control its execution state.
+ */
 public interface Task extends Runnable {
 
+    /**
+     * Returns the job to be executed by this task.
+     *
+     * @return the job to be executed
+     */
     Runnable job();
 
+    /**
+     * Returns the interval in milliseconds between executions of this task.
+     *
+     * @return the interval in milliseconds
+     */
     long interval();
 
+    /**
+     * Checks if the task is currently paused.
+     *
+     * @return true if the task is paused, false otherwise
+     */
     boolean paused();
 
+    /**
+     * Pauses the execution of this task.
+     */
     void pause();
 
+    /**
+     * Resumes the execution of this task if it was paused.
+     */
     void resume();
 
 }
